5e81c9675fe7a1f73047d4a5cec75ec28e5461ee
WeeSee <weesee@web.de>
2018-01-21 19:14:48 +0100

    add proxy class for access to protected members

---
+++ TCPDFLabel.php
@@ -0,0 +1,16 @@
+<?php
+namespace weesee\pdflabel;
+
+/**
+ * PDFLabel
+ * 
+ */
+use yii\base\Component;
+use yii\helpers\Html;
+use Uskur\PdfLabel\PdfLabel as BasePdfLabel;
+
+class TCPDFLabel extends BasePdfLabel
+{
+	
+		
+}
